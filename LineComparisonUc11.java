69382a09dc8bdf1c8423fc3cbaeaf685e8769b1e
DhariyalPTanuja <dhariyal.ptanuja.com>
2022-09-22 08:16:47 +0530

    uploading

--- LineComparisonUc11.java
+++ LineComparisonUc11.java
@@ -1,25 +1,29 @@
-//Line Comparison Computation UC1
 package com.bridgelabs.problemday3;
 
 import java.util.Scanner;
 
-public class LineComparisonUC1 {
+public class LineComparisonUc11 {
 	
-public static void main(String[] args) {
-	System.out.println("Welcome to Line Comparison Computation");
-	getLineCordinate();
-}
+	public LineComparisonUc11() {
+		getLineCordinate();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation");
+		
+		LineComparisonUc11 obj1 = new LineComparisonUc11();
+		
+	}
 	public static void getLineCordinate() {
-		System.out.println("To model a line ");
+		System.out.println("To  a line ");
 		Scanner scanobj = new Scanner(System.in);
-		System.out.println("enter the point P cordinate ");
+		System.out.println("enter the start and end  cordinate of Line l1: ");
 		int x1 = scanobj.nextInt();
 		int y1 = scanobj.nextInt();
-		System.out.println("Point P cordinatec (x ,y) = ( " + x1 + "," + y1 + " )" );
-		System.out.println("enter the point Q cordinate ");
 		int x2 = scanobj.nextInt();
 		int y2 = scanobj.nextInt();
-		System.out.println("Point Q cordinatec (x ,y) = ( " + x2 + "," + y2 + " )" );
+		System.out.println("line l1 start cordinates (x1 ,y1 ) = ( " + x1 + "," + y1 + " )" );
+		System.out.println("line l1  end cordinate (x2 ,y2) = ( " + x2 + "," + y2 + " )" );
 		
 		  int lineLength = calculateLineLength(x1,y1,x2,y2);
 		System.out.println(" length of line: " + lineLength);
@@ -30,6 +34,4 @@ public static void main(String[] args) {
 		 int length = (int) Math.sqrt((Math.pow((x2-x1), 2)) + (Math.pow((y2-y1),2)) );
 		return length;
 	}
-				
 }
-
